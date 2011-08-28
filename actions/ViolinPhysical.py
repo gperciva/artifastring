95e77f8acae6a82354f1d16c912287472716c284
Graham Percival <graham@percival-music.ca>
2011-08-28 02:27:57 +0100

    Change python indentation to 4 spaces.

--- actions/ViolinPhysical.py
+++ actions/ViolinPhysical.py
@@ -6,158 +6,158 @@ BOW_LENGTH = 0.75
 
 
 def semitones(num):
-	return 1.0 - 1.0 / (1.05946309**num)
+    return 1.0 - 1.0 / (1.05946309**num)
 
 
 class ViolinPhysical:
-	"""Creates an .actions file (for both sound synthesis and
-	   visualization in Blender."""
+    """Creates an .actions file (for both sound synthesis and
+       visualization in Blender."""
 
-	def __init__(self, filename_base):
-		""" Constructor """
-		self.seconds_action = 0.0
-		self.seconds_target = 0.0
+    def __init__(self, filename_base):
+        """ Constructor """
+        self.seconds_action = 0.0
+        self.seconds_target = 0.0
 
-		self.actions = open(filename_base+'.actions', 'w')
-		self.actions.write('# type\tseconds\tparams...\n')
+        self.actions = open(filename_base+'.actions', 'w')
+        self.actions.write('# type\tseconds\tparams...\n')
 
-		self.bow_bridge_distance = 0.0
-		self.bow_force = 0.0
-		self.bow_velocity = 0.0
-		self.bow_pos_along = 0.0
-		self.bow_string = 1
-
-
-	def __del__(self):
-		""" Destructor, calls self.end() if necessary. """
-		if self.actions:
-			self.end()
-
-	def end(self):
-		""" Write file and stop. """
-		self.wait(0)
-		self.actions.close()
-		self.actions = None
-
-	def finger(self, vln_string, finger_pos):
-		""" Places finger on string. """
-		vln_string_n = self.whichToNum(vln_string)
-		self.actions.write("f\t%g\t%i\t%g\n" %
-			(self.seconds_action, vln_string_n, finger_pos) )
-
-	def bow(self, vln_string, bridge_distance, force,
-			velocity, starting_bow_pos=-1):
-		""" Moves bow; velocity is assumed to be constant
-		    until changed. """
-		which_string_num = self.whichToNum(vln_string)
-		if (which_string_num != self.bow_string):
-			self.bowStop()
-		self.bow_string = self.whichToNum(vln_string)
-		self.bow_bridge_distance = bridge_distance
-		self.bow_force = force
-		self.bow_velocity = velocity
-		if starting_bow_pos >= 0:
-			self.bow_pos_along = starting_bow_pos
-		self.actions.write(
-			'b\t%g\t%i\t%g\t%g\t%g\t%g\n' %(
-			self.seconds_action,
-			self.bow_string,
-			self.bow_bridge_distance,
-			self.bow_force,
-			self.bow_velocity,
-			self.bow_pos_along
-			) )
-
-	def bowStop(self):
-		""" Sets bow velocity and force to 0.0.
-		    Called automatically when string is plucked. """
-		self.bow_force = 0.0
-		self.bow_velocity = 0.0
-		self.actions.write(
-			'b\t%g\t%i\t%g\t%g\t%g\t%g\n' %(
-			self.seconds_action,
-			self.bow_string,
-			self.bow_bridge_distance,
-			self.bow_force,
-			self.bow_velocity,
-			self.bow_pos_along
-			) )
-
-	def comment(self, text):
-		""" Adds a comment to the .actions file. """
-		self.actions.write('#' + text + '\n')
+        self.bow_bridge_distance = 0.0
+        self.bow_force = 0.0
+        self.bow_velocity = 0.0
+        self.bow_pos_along = 0.0
+        self.bow_string = 1
 
 
-	def pluck(self,which_string, finger_ratio_from_nut,
-			pluck_ratio_from_bridge, pluck_force=1.0):
-		""" Plucks a string. """
-		if (self.bow_force != 0.0):
-			self.bowStop()
-		self.finger(which_string, finger_ratio_from_nut)
-		string_num = self.whichToNum(which_string)
-		self.actions.write("p\t%g\t%i\t%g\t%g\n" %
-			(self.seconds_action, string_num,
-			 pluck_ratio_from_bridge, pluck_force) )
+    def __del__(self):
+        """ Destructor, calls self.end() if necessary. """
+        if self.actions:
+            self.end()
 
-	def whichToNum(self,which_string):
-		""" Internal function which maps strings like
-		    'vl_A' to a string number. """
-		if (which_string >=0) and (which_string <= 3):
-			return which_string
-		if which_string == 'vl_E':
-			string_num = 3
-		elif which_string == 'vl_A':
-			string_num = 2
-		elif which_string == 'vl_D':
-			string_num = 1
-		elif which_string == 'vl_G':
-			string_num = 0
-		else:
-			string_num = -1
-		return string_num
+    def end(self):
+        """ Write file and stop. """
+        self.wait(0)
+        self.actions.close()
+        self.actions = None
 
-	def wait(self, seconds):
-		if (self.bow_velocity == 0.0):
-			# bow not moving
-			self.seconds_target += seconds
-			self.actions.write('w\t%g\n' %(
-				self.seconds_action))
-			self.seconds_action += seconds
-		else:
-			# bow moving, split up into frames
-			self.seconds_target += seconds
-			delta_frames = int((self.seconds_target
-				  - self.seconds_action)
-				* VIDEO_FPS)
-			for i in range(delta_frames):
-				self.seconds_action += 1.0 / VIDEO_FPS
-				self.bow_pos_along += self.bow_velocity / BOW_LENGTH / VIDEO_FPS
-				if ((self.bow_pos_along > 1.0) or
-				    (self.bow_pos_along < 0.0)):
-					print "ERROR: run out of bow, %g seconds!" % self.seconds_action
-				self.actions.write(
-					'b\t%g\t%i\t%g\t%g\t%g\t%g\n' %(
-					self.seconds_action,
-					self.bow_string,
-					self.bow_bridge_distance,
-					self.bow_force,
-					self.bow_velocity,
-					self.bow_pos_along
-					) )
-			remaining_time = seconds - float(delta_frames) / VIDEO_FPS
-			self.bow_pos_along += self.bow_velocity / BOW_LENGTH * remaining_time
+    def finger(self, vln_string, finger_pos):
+        """ Places finger on string. """
+        vln_string_n = self.whichToNum(vln_string)
+        self.actions.write("f\t%g\t%i\t%g\n" %
+            (self.seconds_action, vln_string_n, finger_pos) )
 
-			self.seconds_action += remaining_time
-			self.actions.write(
-				'b\t%g\t%i\t%g\t%g\t%g\t%g\n' %(
-				self.seconds_action,
-				self.bow_string,
-				self.bow_bridge_distance,
-				self.bow_force,
-				self.bow_velocity,
-				self.bow_pos_along
-				) )
-#			self.actions.write('w\t%g\n' %(
-#				self.seconds_action))
+    def bow(self, vln_string, bridge_distance, force,
+            velocity, starting_bow_pos=-1):
+        """ Moves bow; velocity is assumed to be constant
+            until changed. """
+        which_string_num = self.whichToNum(vln_string)
+        if (which_string_num != self.bow_string):
+            self.bowStop()
+        self.bow_string = self.whichToNum(vln_string)
+        self.bow_bridge_distance = bridge_distance
+        self.bow_force = force
+        self.bow_velocity = velocity
+        if starting_bow_pos >= 0:
+            self.bow_pos_along = starting_bow_pos
+        self.actions.write(
+            'b\t%g\t%i\t%g\t%g\t%g\t%g\n' %(
+            self.seconds_action,
+            self.bow_string,
+            self.bow_bridge_distance,
+            self.bow_force,
+            self.bow_velocity,
+            self.bow_pos_along
+            ) )
+
+    def bowStop(self):
+        """ Sets bow velocity and force to 0.0.
+            Called automatically when string is plucked. """
+        self.bow_force = 0.0
+        self.bow_velocity = 0.0
+        self.actions.write(
+            'b\t%g\t%i\t%g\t%g\t%g\t%g\n' %(
+            self.seconds_action,
+            self.bow_string,
+            self.bow_bridge_distance,
+            self.bow_force,
+            self.bow_velocity,
+            self.bow_pos_along
+            ) )
+
+    def comment(self, text):
+        """ Adds a comment to the .actions file. """
+        self.actions.write('#' + text + '\n')
+
+
+    def pluck(self,which_string, finger_ratio_from_nut,
+            pluck_ratio_from_bridge, pluck_force=1.0):
+        """ Plucks a string. """
+        if (self.bow_force != 0.0):
+            self.bowStop()
+        self.finger(which_string, finger_ratio_from_nut)
+        string_num = self.whichToNum(which_string)
+        self.actions.write("p\t%g\t%i\t%g\t%g\n" %
+            (self.seconds_action, string_num,
+             pluck_ratio_from_bridge, pluck_force) )
+
+    def whichToNum(self,which_string):
+        """ Internal function which maps strings like
+            'vl_A' to a string number. """
+        if (which_string >=0) and (which_string <= 3):
+            return which_string
+        if which_string == 'vl_E':
+            string_num = 3
+        elif which_string == 'vl_A':
+            string_num = 2
+        elif which_string == 'vl_D':
+            string_num = 1
+        elif which_string == 'vl_G':
+            string_num = 0
+        else:
+            string_num = -1
+        return string_num
+
+    def wait(self, seconds):
+        if (self.bow_velocity == 0.0):
+            # bow not moving
+            self.seconds_target += seconds
+            self.actions.write('w\t%g\n' %(
+                self.seconds_action))
+            self.seconds_action += seconds
+        else:
+            # bow moving, split up into frames
+            self.seconds_target += seconds
+            delta_frames = int((self.seconds_target
+                  - self.seconds_action)
+                * VIDEO_FPS)
+            for i in range(delta_frames):
+                self.seconds_action += 1.0 / VIDEO_FPS
+                self.bow_pos_along += self.bow_velocity / BOW_LENGTH / VIDEO_FPS
+                if ((self.bow_pos_along > 1.0) or
+                    (self.bow_pos_along < 0.0)):
+                    print "ERROR: run out of bow, %g seconds!" % self.seconds_action
+                self.actions.write(
+                    'b\t%g\t%i\t%g\t%g\t%g\t%g\n' %(
+                    self.seconds_action,
+                    self.bow_string,
+                    self.bow_bridge_distance,
+                    self.bow_force,
+                    self.bow_velocity,
+                    self.bow_pos_along
+                    ) )
+            remaining_time = seconds - float(delta_frames) / VIDEO_FPS
+            self.bow_pos_along += self.bow_velocity / BOW_LENGTH * remaining_time
+
+            self.seconds_action += remaining_time
+            self.actions.write(
+                'b\t%g\t%i\t%g\t%g\t%g\t%g\n' %(
+                self.seconds_action,
+                self.bow_string,
+                self.bow_bridge_distance,
+                self.bow_force,
+                self.bow_velocity,
+                self.bow_pos_along
+                ) )
+#            self.actions.write('w\t%g\n' %(
+#                self.seconds_action))
 
 
--- actions/notes2actions.py
+++ actions/notes2actions.py
@@ -27,156 +27,156 @@ sixteenth = 0.15544 # 30 seconds / 193 16th notes.
 tempo = 30.0/12.0
 
 def suzuki(which_string, factor, direction):
-	force = 1.5
-	force_target = 0.5
-	velocity = 0.0
-	accel = 20.0 * direction
-	target_velocity = 0.5
-	duration = sixteenth * factor
-	current_time = 0.0
-	while ((velocity*direction < target_velocity) and
-			(current_time < duration/2.0)):
-		force -= (force-force_target)/50.0
-		velocity += accel * dtc
-		violin.bow(which_string, bp, force, velocity, 0.6-direction*0.2)
-		violin.wait(dtc)
-		current_time += dtc
-	flat = duration - 2.0*current_time
-	violin.wait(flat)
-	current_time += flat
-	while ((velocity*direction > 0 ) and
-			(current_time < duration)):
-		velocity -= 2.0 * accel * dtc
-		if (velocity*duration) < 0:
-			velocity = 0
-		violin.bow(which_string, bp, force, velocity)
-		violin.wait(dtc)
-		current_time += dtc
-	violin.wait( duration - current_time );
+    force = 1.5
+    force_target = 0.5
+    velocity = 0.0
+    accel = 20.0 * direction
+    target_velocity = 0.5
+    duration = sixteenth * factor
+    current_time = 0.0
+    while ((velocity*direction < target_velocity) and
+            (current_time < duration/2.0)):
+        force -= (force-force_target)/50.0
+        velocity += accel * dtc
+        violin.bow(which_string, bp, force, velocity, 0.6-direction*0.2)
+        violin.wait(dtc)
+        current_time += dtc
+    flat = duration - 2.0*current_time
+    violin.wait(flat)
+    current_time += flat
+    while ((velocity*direction > 0 ) and
+            (current_time < duration)):
+        velocity -= 2.0 * accel * dtc
+        if (velocity*duration) < 0:
+            velocity = 0
+        violin.bow(which_string, bp, force, velocity)
+        violin.wait(dtc)
+        current_time += dtc
+    violin.wait( duration - current_time );
 
 def six(note):
-	if (note < 4):
-		which_string = 'vl_D'
-	else:
-		which_string = 'vl_A'
-	if (note%4 == 0):
-		violin.finger(which_string, semitones(0))
-	elif (note%4 == 1):
-		violin.finger(which_string, semitones(2))
-	elif (note%4 == 2):
-		violin.finger(which_string, semitones(4))
-	elif (note%4 == 3):
-		violin.finger(which_string, semitones(5))
-	else:
-		print "ERROR: bad note"
-	suzuki(which_string, 1, 1)
-	suzuki(which_string, 1, -1)
-	suzuki(which_string, 1, 1)
-	suzuki(which_string, 1, -1)
-	suzuki(which_string, 1.5, 1)
-	violin.wait(sixteenth*0.5)
-	suzuki(which_string, 1.5, -1)
-	violin.wait(sixteenth*0.5)
-	violin.bow(which_string, bp, 1.0, 0.0)
+    if (note < 4):
+        which_string = 'vl_D'
+    else:
+        which_string = 'vl_A'
+    if (note%4 == 0):
+        violin.finger(which_string, semitones(0))
+    elif (note%4 == 1):
+        violin.finger(which_string, semitones(2))
+    elif (note%4 == 2):
+        violin.finger(which_string, semitones(4))
+    elif (note%4 == 3):
+        violin.finger(which_string, semitones(5))
+    else:
+        print "ERROR: bad note"
+    suzuki(which_string, 1, 1)
+    suzuki(which_string, 1, -1)
+    suzuki(which_string, 1, 1)
+    suzuki(which_string, 1, -1)
+    suzuki(which_string, 1.5, 1)
+    violin.wait(sixteenth*0.5)
+    suzuki(which_string, 1.5, -1)
+    violin.wait(sixteenth*0.5)
+    violin.bow(which_string, bp, 1.0, 0.0)
 
 def change_string(old_string, new_string, duration):
-	force_was = 0.49
-	steps = int(duration / dtc)
-	#lighten = -force_was / steps
-	linear = 0.0
-	old_force = force_was
-	new_force = 0.0
-	
-	violin.comment("begin fade-off string")
-	#old_force = 0.1
-	#violin.bow(old_string, bp, old_force, 0.0)
-	violin.bow(old_string, bp, old_force, 0.0)
-	violin.wait(dtc)
-	#return
-	
-	for i in range(steps-1):
-		violin.comment("top loop")
-		#delta = -old_force / 2.0
-		if (old_force > 0.20):
-			delta = -old_force / 8.0
-		else:
-			if linear == 0.0:
-				linear = -old_force / (steps-1-i)
-				lighten = linear
-				violin.comment("change linear: "+str(lighten))
-	#		#delta = -old_force / 10.0
-			delta = lighten
-		lighten = delta
-		violin.comment("change: "+str(lighten))
-		old_force += lighten
-		if (old_force < 0):
-			old_force = 0.0
-		#new_force += delta
-		violin.bow(old_string, bp, old_force, 0.0)
-#		violin.bow(new_string, bp, new_force, 0.0)
-		violin.wait(dtc)
-	violin.bow(old_string, bp, 0.0, 0.0)
+    force_was = 0.49
+    steps = int(duration / dtc)
+    #lighten = -force_was / steps
+    linear = 0.0
+    old_force = force_was
+    new_force = 0.0
+    
+    violin.comment("begin fade-off string")
+    #old_force = 0.1
+    #violin.bow(old_string, bp, old_force, 0.0)
+    violin.bow(old_string, bp, old_force, 0.0)
+    violin.wait(dtc)
+    #return
+    
+    for i in range(steps-1):
+        violin.comment("top loop")
+        #delta = -old_force / 2.0
+        if (old_force > 0.20):
+            delta = -old_force / 8.0
+        else:
+            if linear == 0.0:
+                linear = -old_force / (steps-1-i)
+                lighten = linear
+                violin.comment("change linear: "+str(lighten))
+    #        #delta = -old_force / 10.0
+            delta = lighten
+        lighten = delta
+        violin.comment("change: "+str(lighten))
+        old_force += lighten
+        if (old_force < 0):
+            old_force = 0.0
+        #new_force += delta
+        violin.bow(old_string, bp, old_force, 0.0)
+#        violin.bow(new_string, bp, new_force, 0.0)
+        violin.wait(dtc)
+    violin.bow(old_string, bp, 0.0, 0.0)
 
 def get_naive_string(pitch):
-	if (pitch < 62):
-		return 0
-	elif (pitch < 69):
-		return 1
-	elif (pitch < 76):
-		return 2
-	else:
-		return 3
+    if (pitch < 62):
+        return 0
+    elif (pitch < 69):
+        return 1
+    elif (pitch < 76):
+        return 2
+    else:
+        return 3
 
 def get_finger_naive(pitch):
-	which_string = get_naive_string(pitch)
-	finger_semitones = pitch - (55 + 7*which_string)
-	position = semitones(finger_semitones)
-	return which_string, position
+    which_string = get_naive_string(pitch)
+    finger_semitones = pitch - (55 + 7*which_string)
+    position = semitones(finger_semitones)
+    return which_string, position
 
 
 def write_actions(notes):
-	bow_dir = 1
-	for i, note in enumerate(notes):
-		time = note[0]
-		pitch = note[1]
-		dur = note[2]
+    bow_dir = 1
+    for i, note in enumerate(notes):
+        time = note[0]
+        pitch = note[1]
+        dur = note[2]
 
-		which_string, position = get_finger_naive(pitch)
-		if (i+1 < len(notes)):
-			next_string, next_pos = get_finger_naive(notes[i+1][1])
-		else:
-			next_string = which_string
+        which_string, position = get_finger_naive(pitch)
+        if (i+1 < len(notes)):
+            next_string, next_pos = get_finger_naive(notes[i+1][1])
+        else:
+            next_string = which_string
 
-		violin.finger(which_string, position)
-		if dur == '16':
-			suzuki(which_string, 1, bow_dir)
-		elif dur == '8':
-			suzuki(which_string, 1.5, bow_dir)
-			if which_string == next_string:
-				violin.wait(sixteenth*0.5)
-			else:
-#				violin.waitSeconds(sixteenth*0.25)
-				change_string(which_string,
-					next_string, sixteenth*0.5)
-		bow_dir *= -1
-	violin.end()
+        violin.finger(which_string, position)
+        if dur == '16':
+            suzuki(which_string, 1, bow_dir)
+        elif dur == '8':
+            suzuki(which_string, 1.5, bow_dir)
+            if which_string == next_string:
+                violin.wait(sixteenth*0.5)
+            else:
+#                violin.waitSeconds(sixteenth*0.25)
+                change_string(which_string,
+                    next_string, sixteenth*0.5)
+        bow_dir *= -1
+    violin.end()
 
 
 def main():
-	filename = sys.argv[1]
-	lines = open(filename).readlines()
-	global violin
-	violin = ViolinPhysical.ViolinPhysical(filename.split('.')[0])
-	notes = []
-	for line in lines:
-		splitline = line.split('\t')
-		time = float(splitline[0])
-		pitch = float(splitline[1])
-		dur = splitline[2].rstrip()
-		notes.append( (time, pitch, dur) )
-	write_actions(notes)
-	
+    filename = sys.argv[1]
+    lines = open(filename).readlines()
+    global violin
+    violin = ViolinPhysical.ViolinPhysical(filename.split('.')[0])
+    notes = []
+    for line in lines:
+        splitline = line.split('\t')
+        time = float(splitline[0])
+        pitch = float(splitline[1])
+        dur = splitline[2].rstrip()
+        notes.append( (time, pitch, dur) )
+    write_actions(notes)
+    
 if __name__ == "__main__":
-	main()
+    main()
 
 
