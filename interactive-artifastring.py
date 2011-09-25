39289c37dfa24872b52e09ea02299884e4524a68
Graham Percival <graham@percival-music.ca>
2011-09-25 08:18:24 +0100

    refactor interactive script, use multiprocessing

--- interactive-artifastring.py
+++ interactive-artifastring.py
@@ -26,18 +26,65 @@ import curses
 import numpy
 import math
 
+import multiprocessing
+import time
+
 import pyaudio
 import aubio.aubiowrapper
 
+HOPSIZE = 1024
+NUM_AUDIO_BUFFERS = 4
+
+# for pitch and buffers
+PRINT_EXTRA_DISPLAY = 0
+
+class Parameters():
+    def __init__(self, st=0, fp=0, bp=.08, f=1.0, v=0.4, T=1.0):
+        self.violin_string = st
+        self.finger_position = fp
+        self.bow_position = bp
+        self.force = f
+        self.velocity = v
+        # HACK: kind-of.  Tension isn't really a normal "playing
+        # parameter", but it _is_ still something under a
+        # musician's control.
+        self.tension_relative = T
+
+def violin_process(instrument_number, params_queue, input_audio_queue,
+        output_audio_queue, tension_queue):
+    violin = violin_instrument.ViolinInstrument(instrument_number)
+    buf = monowav.shortArray(HOPSIZE)
+
+    params = params_queue.get()
+    pc = violin.get_physical_constants(params.violin_string)
+    tension_queue.put(pc.T)
+    violin.finger(params.violin_string, params.finger_position)
+    violin.bow(params.violin_string, params.bow_position,
+               params.force, params.velocity)
+
+    while True:
+        while not params_queue.empty():
+            params = params_queue.get()
+            if params.tension_relative != 1.0:
+                pc = violin.get_physical_constants(params.violin_string)
+                pc.T *= params.tension_relative
+                tension_queue.put(pc.T)
+                violin.set_physical_constants(params.violin_string, pc)
+            violin.finger(params.violin_string, params.finger_position)
+            violin.bow(params.violin_string, params.bow_position,
+                params.force, params.velocity)
+        arr = input_audio_queue.get()
+        violin.wait_samples(buf, HOPSIZE)
+        for i in xrange(HOPSIZE):
+            arr[i] = buf[i]
+        output_audio_queue.put(arr)
+
 class InteractiveViolin():
-    violin = None
     audio_stream = None
 
-    violin_string = 0
-    finger_position = 0.0
-    bow_position = 0.08
-    force = 1.0
-    velocity = 0.4
+    params = Parameters()
+
+    params_queue = None
 
     stdscr = None
     row = 5
@@ -50,10 +97,23 @@ class InteractiveViolin():
         self.stdscr = stdscr
         self.audio_stream = audio_stream
 
-        self.violin = violin_instrument.ViolinInstrument(
-            self.instrument_number)
+        self.params_queue = multiprocessing.Queue()
+        self.input_audio_queue = multiprocessing.Queue(
+            maxsize=NUM_AUDIO_BUFFERS)
+        self.output_audio_queue = multiprocessing.Queue(
+            maxsize=NUM_AUDIO_BUFFERS)
+        self.tension_queue = multiprocessing.Queue(maxsize=2)
+        self.violin_process = multiprocessing.Process(
+            target=violin_process,
+            args=(instrument_number, self.params_queue,
+                  self.input_audio_queue,self.output_audio_queue,
+                  self.tension_queue)
+            )
+        self.violin_process.daemon = True
+
         self.stdscr.nodelay(1)
         self.print_help()
+        #self.stdscr.addstr(20, 50, str("%s" % audio_queue))
 
     def print_help(self):
         helpstr = [[
@@ -78,53 +138,64 @@ class InteractiveViolin():
             for i, text in enumerate(line):
                 self.stdscr.addstr(row, 16*i, text)
 
+    def copy_params(self):
+        change = Parameters()
+        change.violin_string = self.params.violin_string
+        change.finger_position = self.params.finger_position
+        change.bow_position = self.params.bow_position
+        change.force = self.params.force
+        change.velocity = self.params.velocity
+        change.tension_relative = self.params.tension_relative
+        return change
+
+    def turn_off_current_string(self):
+        string_off = self.copy_params()
+        string_off.force = 0
+        string_off.velocity = 0
+        self.params_queue.put(string_off)
+
+    def change_tension(self, alter):
+        change = self.copy_params()
+        change.tension_relative = alter
+        self.params_queue.put(change)
+
     def keypress(self, c):
         if c == 'q':
             return False
         if c == 's':
-            # turn off previous
-            self.violin.bow(self.violin_string, 
-                self.bow_position, 0.0, 0.0)
+            self.turn_off_current_string()
             # new
-            self.violin_string += 1
-            if self.violin_string > 3:
-                self.violin_string = 0
-            self.violin.bow(self.violin_string, 
-                self.bow_position, self.force, self.velocity)
+            self.params.violin_string += 1
+            if self.params.violin_string > 3:
+                self.params.violin_string = 0
         if c == 'a':
-            # turn off previous
-            self.violin.bow(self.violin_string, 
-                self.bow_position, self.force, 0.0) # leave force to deaden
+            self.turn_off_current_string()
             # new
-            self.violin_string -= 1
-            if self.violin_string < 0:
-                self.violin_string = 3
-            self.violin.bow(self.violin_string, 
-                self.bow_position, self.force, self.velocity)
+            self.params.violin_string -= 1
+            if self.params.violin_string < 0:
+                self.params.violin_string = 3
         if c == 'f':
-            if self.finger_position == 0.0:
-                self.finger_position = 0.02
+            if self.params.finger_position == 0.0:
+                self.params.finger_position = 0.02
             else:
-                self.finger_position *= 1.1
-            self.violin.finger(self.violin_string, self.finger_position)
+                self.params.finger_position *= 1.1
         if c == 'd':
-            if self.finger_position < 0.02:
-                self.finger_position = 0.00
+            if self.params.finger_position < 0.02:
+                self.params.finger_position = 0.00
             else:
-                self.finger_position /= 1.1
-            self.violin.finger(self.violin_string, self.finger_position)
+                self.params.finger_position /= 1.1
         if c == 't':
-            self.bow_position *= 1.01
+            self.params.bow_position *= 1.01
         if c == 'g':
-            self.bow_position /= 1.01
+            self.params.bow_position /= 1.01
         if c == 'y':
-            self.force *= 1.1
+            self.params.force *= 1.1
         if c == 'h':
-            self.force /= 1.1
+            self.params.force /= 1.1
         if c == 'u':
-            self.velocity *= 1.1
+            self.params.velocity *= 1.1
         if c == 'j':
-            self.velocity /= 1.1
+            self.params.velocity /= 1.1
 
         skip_violin_print = False
         if c == 'z' or c == 'x' or c == 'c' or c == 'v':
@@ -136,13 +207,7 @@ class InteractiveViolin():
                 alter = 1.01
             if c == 'v':
                 alter = 1.1
-
-            pc = self.violin.get_physical_constants(
-                self.violin_string)
-            pc.T *= alter
-            self.violin.set_physical_constants(
-                self.violin_string, pc)
-            self.stdscr.addstr(self.row, 0, str("T=%.3f" % pc.T))
+            self.change_tension(alter)
             skip_violin_print = True
         if c >= '1' and c <= '7':
             skip_violin_print = True
@@ -150,19 +215,24 @@ class InteractiveViolin():
             self.stdscr.addstr(self.row, 0, str("file written"))
 
         if c == ord('b'):
-            self.force /= 1.1
+            self.params.force /= 1.1
+
+        self.params_queue.put(self.params)
         if not skip_violin_print:
             self.stdscr.addstr(self.row, 0, str(
                 "%i\t%.3f\t%.3f\t%.3f\t%.3f" % (
-                self.violin_string, self.finger_position,
-                self.bow_position, self.force, self.velocity)))
-        # next line
-        self.row += 1
-        if self.row > 20:
-            self.row = 5
-        self.stdscr.addstr(self.row, 0, str(" "*40))
-        self.stdscr.move(self.row, 0)
+                self.params.violin_string, self.params.finger_position,
+                self.params.bow_position, self.params.force, self.params.velocity)))
+            # next line
+            self.row += 1
+            if self.row > 20:
+                self.row = 5
+            self.stdscr.addstr(self.row, 0, str(" "*40))
+            self.stdscr.move(self.row, 0)
         return True
+
+    def print_tension(self, tension):
+        self.stdscr.addstr(23, 5, str("T=%.3f" % tension))
 
     def snapshot(self, cat):
         prev_num = self.cats[cat-1]
@@ -188,44 +258,61 @@ class InteractiveViolin():
         mf_file.close()
 
     def main_loop(self):
-        hopsize = 512
         windowsize = 2048
 
-        buf = monowav.shortArray(hopsize)
-        arr = numpy.zeros(hopsize, dtype=numpy.int16)
-    
+        for i in range(NUM_AUDIO_BUFFERS):
+            arr = numpy.zeros(HOPSIZE, dtype=numpy.int16)
+            self.input_audio_queue.put(arr)
+
+        self.params_queue.put(self.params)
+
         pitch_obj = aubio.aubiowrapper.new_aubio_pitchdetection(
-            windowsize, hopsize, 1, 44100,
+            windowsize, HOPSIZE, 1, 44100,
             aubio.aubiowrapper.aubio_pitch_yinfft,
             aubio.aubiowrapper.aubio_pitchm_freq,
             )
-        fvec = aubio.aubiowrapper.new_fvec(hopsize, 1)
+        fvec = aubio.aubiowrapper.new_fvec(HOPSIZE, 1)
         show_pitch = 0
- 
-        self.violin.bow(self.violin_string, self.bow_position,
-            self.force, self.velocity)
+        time_unit = 0.5*HOPSIZE/44100.0
+
+        self.violin_process.start()
+        # wait for a complete queue before progressing
+        while not self.output_audio_queue.full():
+            time.sleep(time_unit)
+
         while True:
             c = self.stdscr.getch()
             if c != -1:
                 if not self.keypress( chr(c) ):
                     break
-                self.violin.bow(self.violin_string, self.bow_position,
-                    self.force, self.velocity)
     
-            self.violin.wait_samples(buf, hopsize)
-    
-            for i in xrange(hopsize):
-                arr[i] = buf[i]
-                aubio.aubiowrapper.fvec_write_sample(
-                    fvec, buf[i], 0, i)
-            self.audio_stream.write( arr.tostring() )
+            while not self.tension_queue.empty():
+                self.print_tension( self.tension_queue.get() )
 
-            pitch = aubio.aubiowrapper.aubio_pitchdetection(
-                pitch_obj, fvec)
-            show_pitch -= 1
-            if show_pitch <= 0:
-                self.stdscr.addstr(23, 50, str("%.1f" % pitch))
-                show_pitch = 20
+            if not self.output_audio_queue.empty():
+                arr = self.output_audio_queue.get()
+                self.audio_stream.write( arr.tostring() )
+            else:
+                time.sleep(time_unit)
+
+            if PRINT_EXTRA_DISPLAY:
+                self.stdscr.addstr(20, 60, str("in %i" %
+                    self.input_audio_queue.qsize()))
+                self.stdscr.addstr(21, 60, str("out %i" %
+                    self.output_audio_queue.qsize()))
+
+                for i in xrange(HOPSIZE):
+                    aubio.aubiowrapper.fvec_write_sample(
+                       fvec, float(arr[i]), 0, i)
+
+                pitch = aubio.aubiowrapper.aubio_pitchdetection(
+                        pitch_obj, fvec)
+                show_pitch -= 1
+                if show_pitch <= 0:
+                    self.stdscr.addstr(23, 50, str("%.1f" % pitch))
+                    show_pitch = 20
+            self.input_audio_queue.put(arr)
+
     
 
 def main(stdscr):
@@ -239,7 +326,8 @@ def main(stdscr):
         rate = 44100,
         channels = 1,
         format = pyaudio.paInt16,
-        output = True
+        output = True,
+        frames_per_buffer=HOPSIZE,
     )
 
     vln = InteractiveViolin(instrument_number, stdscr, audio_stream)
