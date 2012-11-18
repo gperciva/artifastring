4d6dc1592225673fee64c751afe2fa95caee1b23
Graham Percival <graham@percival-music.ca>
2012-11-17 16:18:52 -0800

    Fix make distclean and bump version.

--- research/benchmark.sh
+++ research/benchmark.sh
@@ -3,15 +3,15 @@
 cd build/
 ../actions/unit.py
 # warm up
-/usr/bin/time -o benchmarks.txt -f "%e" ./actions2wav unit.actions
-/usr/bin/time -a -o benchmarks.txt -f "%e" ./actions2wav unit.actions
+/usr/bin/time -o benchmarks.txt -f "%U" ./actions2wav unit.actions
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions
 
 rm benchmarks.txt
 # actual times
 #{ TIMEFORMAT="%E" time ./actions2wav unit.actions ; } 2>> benchmarks.txt
-/usr/bin/time -o benchmarks.txt -f "%e" ./actions2wav unit.actions
-/usr/bin/time -a -o benchmarks.txt -f "%e" ./actions2wav unit.actions
-/usr/bin/time -a -o benchmarks.txt -f "%e" ./actions2wav unit.actions
+/usr/bin/time -o benchmarks.txt -f "%U" ./actions2wav unit.actions
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions
 
 
 cd ..
