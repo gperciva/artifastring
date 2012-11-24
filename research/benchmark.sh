dad6671f7e532517a71fb24f076662d713df7c5e
Graham Percival <graham@percival-music.ca>
2012-11-24 15:10:49 -0800

    Benchmark script: do violin and cello

--- research/benchmark.sh
+++ research/benchmark.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+INST=0
 cd build/
 ../actions/unit.py
 # warm up
-/usr/bin/time -o benchmarks.txt -f "%U" ./actions2wav unit.actions
-/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions
+touch benchmarks.txt
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions $INST
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions $INST
 
 rm benchmarks.txt
+echo "VIOLIN TIMES" > benchmarks.txt
 # actual times
 #{ TIMEFORMAT="%E" time ./actions2wav unit.actions ; } 2>> benchmarks.txt
-/usr/bin/time -o benchmarks.txt -f "%U" ./actions2wav unit.actions
-/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions
-/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions $INST
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions $INST
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions $INST
+
+
+INST=2
+# warm up
+/usr/bin/time -o /dev/null -f "%U" ./actions2wav unit.actions $INST
+/usr/bin/time -o /dev/null -f "%U" ./actions2wav unit.actions $INST
+
+echo "CELLO TIMES" >> benchmarks.txt
+# actual times
+#{ TIMEFORMAT="%E" time ./actions2wav unit.actions ; } 2>> benchmarks.txt
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions $INST
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions $INST
+/usr/bin/time -a -o benchmarks.txt -f "%U" ./actions2wav unit.actions $INST
+
 
 
 cd ..
